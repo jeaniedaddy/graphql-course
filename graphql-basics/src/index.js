52f67be62bf9fbb34744fbe88d740d58e3827fe8
steve <jeaniedaddy@gmail.com>
2019-06-17 14:11:17 -0400

    14. Creating Custom Types

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,33 +2,46 @@ import { GraphQLServer } from 'graphql-yoga';
 
 // Scalar types:  String, Boolean, Int, Float, ID
 
+// type definitions (schema)
 const typeDefs = ` 
     type Query {
-        title: String!
-        price: Float!
-        releaseYear: Int!
-        rating: Float
-        inStock: Boolean!
+        me: User!
+        post: Post!
     } 
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+    }
 `;
 
 const resolvers = {
     Query: {
-        title(){
-            return 'Jussasic Park II';
+        me(){
+            return {
+                id: '1234OWEB',
+                name: 'steve no',
+                email: 'jeaniedaddy@gmail.com',
+            }
         },
-        price(){
-            return  12.5;
-        },
-        releaseYear(){
-            return 2000; 
-        },
-        rating(){
-            return  null;
-        },
-        inStock(){
-            return true;
-        },
+        post(){
+            return {
+                id: '22222',
+                title: 'My first Post',
+                body: 'this is my post test',
+                published: true
+            }
+        }
+
     }
 };
 
