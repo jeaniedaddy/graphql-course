9b2bf8b7bc033325433e944dbc0622ef6555eb9e
steve <jeaniedaddy@gmail.com>
2019-06-19 15:58:26 -0400

    22. Comment Challenge: Part III

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -9,28 +9,34 @@ import { ProvidedRequiredArguments } from 'graphql/validation/rules/ProvidedRequ
 const comments = [{
     id: '1',
     text: 'good comment',
-    author: '1'
+    author: '1',
+    post: '3'
 }, {
     id: '1',
     text: 'it was really funn',
-    author: '1'
+    author: '1',
+    post: '2'
 }, {
     id: '2',
     text: 'not good',
-    author: '3'
+    author: '3',
+    post: '3'
 }, {
     id: '2',
     text: 'it was so so ',
-    author: '2'
+    author: '2',
+    post: '1'
 
 }, {
     id: '3',
     text: 'waste of my money',
-    author: '1'
+    author: '1',
+    post: '1'
 }, {
     id: '3',
     text: 'amazing',
-    author: '3'
+    author: '3',
+    post: '2'
 }];
 
 const users = [{
@@ -95,12 +101,14 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
 
     type Comment {
         id: ID!
         text: String!
         author: User!
+        post: Post!
     }
 `;
 
@@ -152,6 +160,11 @@ const resolvers = {
             return users.find((user)=>{
                 return user.id === parent.author;
             });
+        },
+        comments(parent, args, ctx, info){
+            return comments.filter((comment)=>{
+                return comment.post === parent.id;
+            });
         }
     },
     User: {
@@ -165,7 +178,11 @@ const resolvers = {
     Comment: {
         author(parent, args, ctx, info){
             return users.find((user)=>(user.id === parent.author));
-        }
+        },
+        post(parent, args, ctx, info){
+            return posts.find((post)=> (post.id === parent.post));
+        } 
+
     }
 };
 
