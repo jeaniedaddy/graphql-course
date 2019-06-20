f0e63b5bd97dc1cdd1ba21ed8f894eb7de7def9e
steve <jeaniedaddy@gmail.com>
2019-06-20 11:20:48 -0400

    27. The Input Type

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -87,7 +87,7 @@ const typeDefs = `
 
     type Mutation {
         createUser(name: String!, email: String!, age: Int): User!
-        createPost(title: String!, body:String!, published: Boolean, author:ID!):Post!
+        createPost(title: String!, body:String!,  author:ID!):Post!
         createComment(post: ID!, text:String!, author:ID!): Comment!
     }
 
@@ -168,46 +168,40 @@ const resolvers = {
             }
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age 
+                ...args
             };
 
             users.push(user);
             return user; 
         },
         createPost(parent, args, ctx, info){
-            const userExist = users.some(user=> user.id === args.author);
+            const userExists = users.some(user=> user.id === args.author);
 
-            if(!userExist){
+            if(!userExists){
                 throw new Error("No user");
             }
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                published: true,
+                ...args
             };
 
             posts.push(post);
             return post; 
         },
         createComment(parent, args, ctx, info){
-            const userExist = users.some(user=> user.id === args.author);
-            const postExist = posts.some(post=> post.id === args.post);
+            const userExists = users.some(user=> user.id === args.author);
+            const postExists = posts.some(post=> post.id === args.post && post.published );
         
-            if(!userExist){
-                throw new Error("No user");
+            if(!userExists){
+                throw new Error("Unable to find the user");
             }
-            if(!postExist){
-                throw new Error("No Post");
+            if(!postExists){
+                throw new Error("Unable to find the post");
             }
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                author: args.author,
-                post: args.post 
+                ...args
             };
 
             comments.push(comment);
