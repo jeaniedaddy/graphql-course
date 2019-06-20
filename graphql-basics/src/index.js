fbc8cffbf7bc4f110462b2393a08ad9afaeed16e
steve <jeaniedaddy@gmail.com>
2019-06-20 10:47:38 -0400

    25. Creating Data with Mutations: Part II

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -87,6 +87,8 @@ const typeDefs = `
 
     type Mutation {
         createUser(name: String!, email: String!, age: Int): User!
+        createPost(title: String!, body:String!, published: Boolean, author:ID!):Post!
+        createComment(post: ID!, text:String!, author:ID!): Comment!
     }
 
     type User {
@@ -160,8 +162,9 @@ const resolvers = {
     },
     Mutation : {
         createUser(parent, args, ctx, info){
-            if(users.some(user=> user.email.toLowerCase()=== args.email.toLowerCase())){
-                throw new Error("email taken");
+            const emailTaken = users.some(user=> user.email.toLowerCase()=== args.email.toLowerCase());
+            if(emailTaken){
+                throw new Error("Email taken");
             }
             const user = {
                 id: uuidv4(),
@@ -172,7 +175,45 @@ const resolvers = {
 
             users.push(user);
             return user; 
-        }
+        },
+        createPost(parent, args, ctx, info){
+            const userExist = users.some(user=> user.id === args.author);
+
+            if(!userExist){
+                throw new Error("No user");
+            }
+            const post = {
+                id: uuidv4(),
+                title: args.title,
+                body: args.body,
+                published: args.published,
+                author: args.author
+            };
+
+            posts.push(post);
+            return post; 
+        },
+        createComment(parent, args, ctx, info){
+            const userExist = users.some(user=> user.id === args.author);
+            const postExist = posts.some(post=> post.id === args.post);
+        
+            if(!userExist){
+                throw new Error("No user");
+            }
+            if(!postExist){
+                throw new Error("No Post");
+            }
+            const comment = {
+                id: uuidv4(),
+                text: args.text,
+                author: args.author,
+                post: args.post 
+            };
+
+            comments.push(comment);
+            return comment; 
+        },
+
     },
     Post: {
         author(parent, args, ctx, info){
