43efe336b6e322a3cdca59d6c58b85e1d72f249d
steve <jeaniedaddy@gmail.com>
2019-07-06 11:08:46 -0400

    add graphql prisma

---
+++ graphql-prisma/src/index.js
@@ -0,0 +1,24 @@
+import { GraphQLServer } from 'graphql-yoga';
+import { PubSub } from 'graphql-yoga'
+
+import Query from './resolvers/Query'
+import Mutation from './resolvers/Mutation'
+import Post from './resolvers/Post'
+import User from './resolvers/User'
+import Comment from './resolvers/Comment'
+import Subscription from './resolvers/Subscription'
+import db from './db';
+import './prisma'
+
+const pubsub = new PubSub();
+const server = new GraphQLServer({
+    typeDefs: "./src/schema.graphql", 
+    resolvers: {
+        Query, Mutation, Subscription, Post, User, Comment
+    },
+    context: { db, pubsub }
+});
+
+server.start((res)=>{
+    console.log("Server is up at port : " + res.port);
+}); 
---
+++ graphql-prisma/src/prisma.js
@@ -0,0 +1,119 @@
+import { Prisma } from 'prisma-binding'
+
+
+const prisma = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: 'http://192.168.99.100:4466/reviews/default',
+})
+
+export { prisma as default }
+
+// prisma.exists.User({id: "cjxksrd8u001107770nl4bunp"}).then((exists)=>{
+//     console.log(exists)   
+// })
+
+
+// const createPostForUser = async (userId, data)=> {
+//     const userExists = await prisma.exists.User({id: userId})
+
+//     if(!userExists){
+//         throw new Error("User not found")
+//     }
+
+//     const post =  await prisma.mutation.createPost({
+//         data: {
+//             ...data,
+//             author: {
+//                 connect: {
+//                     id: userId
+//                 }
+//             }
+//         }
+//     },'{ id title body published author { id name email posts { id title published } }}')
+
+//     return post.author
+// }
+
+// createPostForUser("dddddd", {
+//     title: "want to see john wick",
+//     body: "john wick",
+//     published: false
+// }).then((data)=>{
+//     console.log(data)
+// }).catch((error)=>{
+//     console.log("....." + error.message)
+// })
+
+// const updatePostForUser= async (postId, data) => {
+//     const postExists = await prisma.exists.Post({id: postId})
+//     if(!postExists){
+//         throw new Error("Post not found")
+//     }
+
+//     const post = await prisma.mutation.updatePost({
+//         data, 
+//         where: {
+//             id: postId
+//         }
+//     },' { id title body published author { id name email posts { id title } } }')
+    
+//     return post.author
+// }
+
+// updatePostForUser("cjxlvfmn2004l0777uigr67q0", {
+//     title: "g again again",
+//     published: true
+// } ).then((data)=>{
+//     console.log(JSON.stringify(data, undefined, 2))
+// }).catch((error)=>{
+//     console.log(error.message)
+// })
+
+
+
+
+// prisma.query.users(null, '{ id name posts { id title body } }').then((data)=>{
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+
+// prisma.query.comments(null, '{ id text post { title } author { id email } }').then(data=>{
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+// 
+// prisma.query.posts(null, '{ id  title body published author { name }}').then(data=>{
+//     console.log(JSON.stringify(data, undefined, 2))
+// })
+
+
+// prisma.mutation.createPost({
+//     data: {
+//         title: "22222 this is graphql post",
+//         body: "GraphQL 202",
+//         published: false,
+//         author: {
+//             connect: {
+//                 id: "cjxlw8ai8006u0777ug07hatc"
+//             }
+//         }
+//     }
+// }, ' { id title body published author { id name } }').then((data)=>{
+//     console.log(data); 
+//     return prisma.query.posts(null, ' {id title body published author { name } } ')
+// }).then((data)=> {
+//     console.log(data); 
+// })
+
+// prisma.mutation.updatePost({
+//     data: {
+//         body: 'GRAPHQL 202',
+//         published: true
+//     },
+//     where: {
+//         id: "cjxm51qrt008w077791sjngtv"
+//     }
+// }, '{ id title body published }').then((data)=>{
+//     console.log(data)
+//     return prisma.query.posts(null, ' { id title, body, published }')
+// }).then((data) => {
+//     console.log(data)
+// })
