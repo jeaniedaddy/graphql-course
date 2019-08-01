8664dbb3149e816ab3ecffff6fd48e2b8a800f10
steve <jeaniedaddy@gmail.com>
2019-08-01 14:55:32 -0400

    fixed 77.Fragments

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,23 +1,25 @@
 import getUserId from '../utils/getUserId'
 
 const User = {
+    posts: {
+        fragment: ' fragment userId on User { id } ',
+        resolve(parent, args, { prisma, request }, info){
+            return prisma.query.posts({
+                where: {
+                    published: true,
+                    author: {
+                        id: parent.id
+                    }
+                }
+            })
+        }
+    },
     email: {
         fragment: ' fragment userId on User { id } ',
         resolve(parent, args, { request }, info){
             const userId = getUserId(request, false)
             if(userId && parent.id === userId){
                 return parent.email
-            } else {
-                return null
-            }
-        }
-    },
-    posts: {
-        fragment: ' fragment userId on User { id } ',
-        resolve(parent, args, { request }, info){
-            const userId = getUserId(request, false)
-            if(userId && parent.id === userId){
-                return parent.posts
             } else {
                 return null
             }
